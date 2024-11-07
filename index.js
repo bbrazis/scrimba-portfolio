472002186b3eda50de7c59dd155c0def88721b43
bbrazis <32001385+bbrazis@users.noreply.github.com>
2024-11-07 15:24:05 -0800

    New Content

--- index.js
+++ index.js
@@ -4,13 +4,16 @@ const navMenu = document.querySelector('#navMenu')
 const headerHamMenuBtn = hamMenuBtn.querySelector('.main-ham-menu')
 const headerHamMenuCloseBtn = hamMenuBtn.querySelector('.main-ham-menu-close')
 const headerNavMenuLinks = navMenu.querySelectorAll('a')
+const headerClose = document.querySelector('.blurry-close')
 
 hamMenuBtn.addEventListener('click', () => {
   if (navMenu.classList.contains('menu--active')) {
     navMenu.classList.toggle('menu--active')
+    document.body.classList.toggle('menu-open')
   } else {
     navMenu.classList.toggle('menu--active')
-    navMenu.focus({ preventScroll:true })
+    document.body.classList.toggle('menu-open')
+    navMenu.focus({ preventScroll: true })
   }
   if (headerHamMenuBtn.classList.contains('d-none')) {
     headerHamMenuBtn.classList.toggle('d-none')
@@ -25,11 +28,14 @@ function closeMenu() {
     navMenu.classList.toggle('menu--active')
     headerHamMenuBtn.classList.toggle('d-none')
     headerHamMenuCloseBtn.classList.toggle('d-none')
+    document.body.classList.toggle('menu-open')
 }
 
 for (let link of headerNavMenuLinks) {
   link.addEventListener('click', closeMenu)
 }
+
+headerClose.addEventListener('click', closeMenu)
 
 // // ---
 // const headerLogoConatiner = document.querySelector('.header__logo-container')
